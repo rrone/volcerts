e5dee94322e9eeb9e18d357bcd79a30f82729cb1
Rick Roberts <web.ayso@gmail.com>
2019-03-01 11:27:14 -0800

    Version 2019.02.28.50:  - Modified VolCerts to retrieve multiple id in GET request

--- src/Controller/VolCertsFormController.php
+++ src/Controller/VolCertsFormController.php
@@ -6,9 +6,15 @@ use App\Service\VolCerts;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class VolCertsFormController extends AbstractController
 {
+    /**
+     * @var RequestStack $requestStack
+     */
+    private $requestStack;
+
     /**
      * @var string $appVersion
      */
@@ -26,13 +32,16 @@ class VolCertsFormController extends AbstractController
 
     /**
      * VolCertsFormController constructor
+     * @param RequestStack $requestStack
      * @param string $appVersion
      * @param string $projectDir
      * @param VolCerts $volCerts
      */
 
-    public function __construct(string $appVersion, string $projectDir, VolCerts $volCerts)
+    public function __construct(RequestStack $requestStack, string $appVersion, string $projectDir, VolCerts $volCerts)
     {
+        $this->requestStack = $requestStack;
+
         $this->appVersion = $appVersion;
 
         $this->projectDir = $projectDir;
@@ -62,8 +71,12 @@ class VolCertsFormController extends AbstractController
      */
     public function get($id)
     {
+        $request = $this->requestStack->getCurrentRequest();
+
+        $content = explode(',', $request->get('id'));
+
         $response = new JsonResponse(
-            $this->volCerts->retrieveVolCertData($id),
+            $this->volCerts->retrieveVolsCertData($content),
             JsonResponse::HTTP_OK
         );
 
