1a75522fce3ba787252b69c50ee71de1a8a8eef9
rick <ayso1sra@gmail.com>
2021-06-30 11:36:48 -0700

    Version 2021.06.30.00:  - Updated composer to Symfony 5.3.3  - Removed unused vars in Controllers

--- src/Controller/VolCertsFormController.php
+++ src/Controller/VolCertsFormController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class VolCertsFormController
@@ -15,31 +13,23 @@ use Symfony\Component\HttpFoundation\Request;
 class VolCertsFormController extends AbstractController
 {
     /**
-     * @var Request $request
-     */
-    private $request;
-
-    /**
      * @var string $appVersion
      */
-    private $appVersion;
+    private string $appVersion;
 
     /**
      * @var string $projectDir
      */
-    private $projectDir;
+    private string $projectDir;
 
     /**
      * VolCertsFormController constructor
-     * @param RequestStack $requestStack
      * @param  $appVersion
      * @param  $projectDir
      */
 
-    public function __construct(RequestStack $requestStack, $appVersion, $projectDir)
+    public function __construct($appVersion, $projectDir)
     {
-        $this->request = $requestStack->getCurrentRequest();
-
         $this->appVersion = $appVersion;
 
         $this->projectDir = $projectDir;
@@ -50,7 +40,7 @@ class VolCertsFormController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render(
             'form.html.twig',
